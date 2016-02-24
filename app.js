c92780dcceb465a1e1c2263a7f27d6b80d6e89cf
tomasro27 <tomasrodriguez@utexas.edu>
2016-02-23 21:05:37 -0600

    app setup2

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
+var exphbs = require('express-handlebars');
+var path = require('path');
 
 app.use('/', require('./routes/index'));
 
+// view engine setup - currently uses Handlebars
+app.set('views', path.join(__dirname, 'views'));
+
+var hbs = exphbs.create({
+	helpers: require('./helpers/handlebars.js'),
+	extname: '.hbs',
+	layoutsDir: './views/',
+	defaultLayout: 'layout'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
 
 var port = process.env.PORT || 3000;
 http.listen(port, function(){
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
 	res.render('index', {
-		title: 'Inicio'
+		title: 'MedicinasVE'
 	});
 });
 
